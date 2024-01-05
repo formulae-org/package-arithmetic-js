292bb9a44d5ef0b7631312fae1234457262291cf
Laurence R. Ugalde <laurence.ugalde@formulae.org>
2024-01-04 20:42:25 -0600

    02024-01-04

--- reduction.js
+++ reduction.js
@@ -308,6 +308,7 @@ Arithmetic.additionNegativeAddition = async (addition, session) => {
 	
 	if (updates > 0) {
 		//session.log("Addend of negative addition reduces to negative addends");
+		await session.reduce(addition);
 		return true;
 	}
 	
@@ -2922,6 +2923,279 @@ Arithmetic.isPrime = async (isPrime, session) => {
 	return true;
 };
 
+/**
+	Complex conjugate of an expression
+		
+	number*    ->   number   (decimal, integer of rational)
+	i*         ->   i
+	(a + b)*   ->   a* + b*
+	(a b)*     ->   a* b*
+	(a / b)*   ->   a* / b*
+	(-a)*      ->   -(a*)
+ */
+ 
+Arithmetic.conjugate = async (conjugate, session) => {
+	let e = conjugate.children[0];
+	
+	if (CanonicalArithmetic.isExpressionCanonicalNumeric(e)) {
+		conjugate.replaceBy(e);
+		//session.log("Conjugate of a number is the same number");
+		return true;
+	}
+	
+	let tag = e.getTag();
+	switch (tag) {
+		case "Math.Complex.Imaginary": {
+			let result = Formulae.createExpression("Math.Arithmetic.Negative");
+			result.addChild(e);
+			conjugate.replaceBy(result);
+			//session.log("Conjugate of a imaginary number");
+			session.reduce(result);
+			return true;
+		}
+		
+		case "Math.Arithmetic.Addition":
+		case "Math.Arithmetic.Multiplication":
+		case "Math.Arithmetic.Division":
+		case "Math.Arithmetic.Negative": {
+			let conj;
+			
+			for (let i = 0, n = e.children.length; i < n; ++i) {
+				conj = Formulae.createExpression("Math.Complex.Conjugate");
+				conj.addChild(e.children[i]);
+				e.setChild(i, conj);
+			}
+			conjugate.replaceBy(e);
+			//session.log("Conjugation of basic operations");
+			
+			for (let i = 0, n = e.children.length; i < n; ++i) await session.reduce(e.children[i]);
+			await session.reduce(e);
+			
+			return true;
+		}
+		
+		case "Math.Complex.Conjugate": {
+			conjugate.replaceBy(e.children[0]);
+			//session.log("Conjugate of a conjugate is cancelled");
+			return true;
+		}
+	}
+	
+	return false;
+};
+
+/**
+	Searches for imaginary unit in a multiplication
+	If more than one found, subsitutes them by i, -i, 1 or -1
+ */
+
+Arithmetic.productContainingI = async (multiplication, session) => {
+	let pos, n = multiplication.children.length;
+	let occurrences = 0;
+	
+	for (pos = 0; pos < n; ++pos) {
+		if (multiplication.children[pos].getTag() === "Math.Complex.Imaginary") {
+			occurrences = 1;
+			break;
+		}
+	}
+	
+	// there was not any imaginary unit
+	if (pos >= n) return false; // forward to other forms of Multiplication(...)
+	
+ 	// there was, index is (pos)
+	
+	// performs multiplication with other numeric addends
+	
+	for (let i = n - 1; i > pos; --i) {
+		if (multiplication.children[i].getTag() === "Math.Complex.Imaginary") {
+			multiplication.removeChildAt(i);
+			++occurrences;
+		}
+	}
+	
+	// only one occurrence
+	if (occurrences === 1) return false;
+	
+	switch (occurrences % 4) {
+		case 0:
+			multiplication.children[pos].replaceBy(CanonicalArithmetic.number2Expr(1));
+			break;
+		
+		case 1:
+			// does noting, it is already the imaginary unit
+			break;
+		
+		case 2:
+			multiplication.children[pos].replaceBy(CanonicalArithmetic.number2Expr(-1));
+			break;
+		
+		case 3: {
+				let neg = Formulae.createExpression("Math.Arithmetic.Negative");
+				let i = multiplication.children[pos];
+				multiplication.setChild(pos, neg);
+				neg.addChild(i);
+			}
+			break;
+	}
+	
+	if (multiplication.children.length == 1) { // just one child
+		multiplication.replaceBy(multiplication.children[0]);
+	}
+	
+	return true;
+};
+
+let containsI = (expr, testing) => {
+	let negative = false;
+	let tag = expr.getTag();
+	
+	if (tag === "Math.Complex.Imaginary") return testing ? true : new CanonicalArithmetic.Integer(1);
+	
+	if (tag === "Math.Arithmetic.Negative") {
+		expr = expr.children[0];
+		tag = expr.getTag();
+		if (tag === "Math.Complex.Imaginary") return testing ? true : new CanonicalArithmetic.Integer(-1);
+		negative = true;
+	}
+	
+	if (tag === "Math.Arithmetic.Multiplication") {
+		if (expr.children.length !== 2) return false;
+		
+		if (expr.children[0].getTag() === "Math.Complex.Imaginary") {
+			if (testing) {
+				return CanonicalArithmetic.isExpressionCanonicalNumeric(expr.children[1]);
+			}
+			else {
+				let n = CanonicalArithmetic.expr2CanonicalNumeric(expr.children[1]);
+				if (negative) n = n.negate();
+				return n;
+			}
+		}
+		
+		if (expr.children[1].getTag() === "Math.Complex.Imaginary") {
+			if (testing) {
+				return CanonicalArithmetic.isExpressionCanonicalNumeric(expr.children[0]);
+			}
+			else {
+				let n = CanonicalArithmetic.expr2CanonicalNumeric(expr.children[0]);
+				if (negative) n = n.negate();
+				return n;
+			}
+		}
+	}
+	
+	return false;
+};
+
+Arithmetic.additionContainingI = async (addition, session) => {
+	let pos, n = addition.children.length;
+	
+	for (pos = 0; pos < n; ++pos) {
+		if (containsI(addition.children[pos], true)) {
+			break;
+		}
+	}
+	
+	// there was not any imaginary unit
+	if (pos >= n) return false; // forward to other forms of Multiplication(...)
+	
+ 	// there was, index is (pos)
+	
+	// performs addition with other numeric proportions
+	
+	let sum = null;
+	
+	for (let i = n - 1; i > pos; --i) {
+		if (containsI(addition.children[i], true)) {
+			if (sum === null) {
+				sum = containsI(addition.children[pos], false);
+			}
+			
+			sum = sum.addition(containsI(addition.children[i], false), session);
+			addition.removeChildAt(i);
+		}
+	}
+	
+	// were there changes ?
+	if (sum === null) return false;
+	
+	if (sum.isZero()) {
+		if (addition.children.length >= 2) {
+			addition.removeChildAt(pos);
+		}
+		else {
+			let r = CanonicalArithmetic.canonicalNumeric2Expr(sum);
+			addition.children[0].replaceBy(r);
+		}
+	}
+	else {
+		if (sum.isPositive()) {
+			if (sum.isOne()) {
+				let r = Formulae.createExpression("Math.Complex.Imaginary");
+				addition.children[pos].replaceBy(r);
+			}
+			else {
+				let r = Formulae.createExpression("Math.Arithmetic.Multiplication");
+				r.addChild(CanonicalArithmetic.canonicalNumeric2Expr(sum));
+				r.addChild(Formulae.createExpression("Math.Complex.Imaginary"));
+				addition.children[pos].replaceBy(r);
+			}
+		}
+		else {
+			sum = sum.negate();
+			let r = Formulae.createExpression("Math.Arithmetic.Negative");
+			if (sum.isOne()) {
+				r.addChild(Formulae.createExpression("Math.Complex.Imaginary"))
+				addition.children[pos].replaceBy(r);
+			}
+			else {
+				let m = Formulae.createExpression("Math.Arithmetic.Multiplication");
+				m.addChild(CanonicalArithmetic.canonicalNumeric2Expr(sum));
+				m.addChild(Formulae.createExpression("Math.Complex.Imaginary"));
+				r.addChild(m);
+				addition.children[pos].replaceBy(r);
+			}
+		}
+	}
+	
+	if (addition.children.length == 1) { // just one child
+		addition.replaceBy(addition.children[0]);
+	}
+	
+	return true;
+};
+
+Arithmetic.multiplicationDistributiveOverAddition = async (multiplication, session) => {
+	for (let i = 0, n = multiplication.children.length; i < n; ++i) {
+		if (multiplication.children[i].getTag() === "Math.Arithmetic.Addition") {
+			let factors = multiplication.children[i];
+			
+			let a = Formulae.createExpression("Math.Arithmetic.Addition");
+			let m;
+			let j;
+			
+			for (let f = 0, F = factors.children.length; f < F; ++f) {
+				m = Formulae.createExpression("Math.Arithmetic.Multiplication");
+				m.addChild(factors.children[f].clone());
+				
+				for (j = 0; j < n; ++j) {
+					if (j === i) continue;
+					m.addChild(multiplication.children[j].clone());
+				}
+				
+				a.addChild(m);
+			}
+			
+			multiplication.replaceBy(a);
+			await session.reduce(a);
+			return true;
+		}
+	}
+	
+	return false;
+};
+
 Arithmetic.setReducers = () => {
 	// precision
 	
@@ -2931,8 +3205,6 @@ Arithmetic.setReducers = () => {
 	ReductionManager.addReducer("Math.Arithmetic.WithPrecision",     Arithmetic.withPrecision,     "Arithmetic.withPrecision", { special: true });
 	
 	// rounding
-	
-	
 	
 	// rounding mode
 	
@@ -2949,13 +3221,14 @@ Arithmetic.setReducers = () => {
 	ReductionManager.addReducer("Math.Numeric", ReductionManager.expansionReducer, "ReductionManager.expansionReducer", { precedence: ReductionManager.PRECEDENCE_LOW});
 	
 	ReductionManager.addReducer("Math.Arithmetic.Addition", Arithmetic.additionNumeric,          "Arithmetic.additionNumeric");
-	ReductionManager.addReducer("Math.Arithmetic.Addition", ReductionManager.itselfReducer,      "ReductionManager.itselfReducer",      { symbolic: true });
-	ReductionManager.addReducer("Math.Arithmetic.Addition", Arithmetic.additionNegativeAddition, "Arithmetic.additionNegativeAddition", { symbolic: true });
+	ReductionManager.addReducer("Math.Arithmetic.Addition", ReductionManager.itselfReducer,      "ReductionManager.itselfReducer",      { symbolic: false });
+	ReductionManager.addReducer("Math.Arithmetic.Addition", Arithmetic.additionNegativeAddition, "Arithmetic.additionNegativeAddition", { symbolic: false });
 	
 	ReductionManager.addReducer("Math.Arithmetic.Multiplication", Arithmetic.multiplicationNumeric,         "Arithmetic.multiplicationNumeric");
-	ReductionManager.addReducer("Math.Arithmetic.Multiplication", ReductionManager.itselfReducer,           "ReductionManager.itselfReducer", { symbolic: true });
+	ReductionManager.addReducer("Math.Arithmetic.Multiplication", ReductionManager.itselfReducer,           "ReductionManager.itselfReducer", { symbolic: false });
 	ReductionManager.addReducer("Math.Arithmetic.Multiplication", Arithmetic.multiplicationNegatives,       "Arithmetic.multiplicationNegatives", { symbolic: false });
 	ReductionManager.addReducer("Math.Arithmetic.Multiplication", Arithmetic.multiplicationNumericAddition, "Arithmetic.multiplicationNumericAddition", { symbolic: true });
+	ReductionManager.addReducer("Math.Arithmetic.Multiplication", Arithmetic.multiplicationDistributiveOverAddition, "Arithmetic.multiplicationDistributiveOverAddition", { symbolic: false });
 	
 	ReductionManager.addReducer("Math.Arithmetic.Division", Arithmetic.divisionNegatives,            "Arithmetic.divisionNegatives", { symbolic: true });
 	ReductionManager.addReducer("Math.Arithmetic.Division", Arithmetic.divisionZeroOne,              "Arithmetic.divisionZeroOne");
@@ -3070,7 +3343,7 @@ Arithmetic.setReducers = () => {
 	ReductionManager.addReducer("Math.Arithmetic.Random",        Arithmetic.random,        "Arithmetic.random");
 	ReductionManager.addReducer("Math.Arithmetic.RandomInRange", Arithmetic.randomInRange, "Arithmetic.randomInRange");
 	
-	ReductionManager.addReducer("Math.Arithmetic.Piecewise", Arithmetic.piecewise, "Arithmetic.piecewise");
+	ReductionManager.addReducer("Math.Arithmetic.Piecewise", Arithmetic.piecewise, "Arithmetic.piecewise", { special: true });
 	
 	ReductionManager.addReducer("Math.Arithmetic.Summation", Arithmetic.summationProductReducer,     "Arithmetic.summationProductReducer", { special: true });
 	ReductionManager.addReducer("Math.Arithmetic.Summation", Arithmetic.summationProductListReducer, "Arithmetic.summationProductListReducer", { special: true });
@@ -3078,4 +3351,10 @@ Arithmetic.setReducers = () => {
 	ReductionManager.addReducer("Math.Arithmetic.Product",   Arithmetic.summationProductListReducer, "Arithmetic.summationProductListReducer", { special: true });
 	
 	ReductionManager.addReducer("Math.Arithmetic.IsPrime", Arithmetic.isPrime, "Arithmetic.isPrime");
+	
+	// complex arithmetic
+	
+	ReductionManager.addReducer("Math.Complex.Conjugate",         Arithmetic.conjugate,           "Arithmetic.conjugate");
+	ReductionManager.addReducer("Math.Arithmetic.Multiplication", Arithmetic.productContainingI,  "Arithmetic.productContainingI");
+	ReductionManager.addReducer("Math.Arithmetic.Addition",       Arithmetic.additionContainingI, "Arithmetic.additionContainingI");
 };
